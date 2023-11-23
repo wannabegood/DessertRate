1274c49174af21582c7652e8a306e8ff945c5ba6
Greg Fulton <greg.fulton@saic.com>
2023-11-23 03:51:00 -0500

    separate into cs backing-page

---
+++ Pages/Counter.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace DessertRate.Pages;
+public class CounterBase : ComponentBase
+{
+
+    protected int CurrentCount = 0;
+
+    protected void IncrementCount()
+    {
+        CurrentCount++;
+    }
+
+
+}
